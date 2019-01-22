270e23e97b9692cb799e2514c9578c220684e3f9
Yanchu Wang <yw5mj@virginia.edu>
2019-01-22 02:02:43 -0500

    start parsing

---
+++ beeradv_crawler/beer_brew_style_idmapping.py
@@ -0,0 +1,50 @@
+#! /bin/env python
+# script to map IDs for brewery/beer/style
+import os,csv
+class id_mapping(object):
+    def __init__(self,indir="data_html",outdir="data_csv"):
+        '''
+        initialization
+        '''
+        self.indir=indir
+        os.system('mkdir -p '+outdir)
+        self.outdir=outdir
+
+    def style_map(self,filename='style_id'):
+        '''
+        mapping style IDs
+        '''
+        outf=open('{0}/{1}.csv'.format(self.outdir,filename),'w')
+        idwrt=csv.writer(outf,delimiter=';')
+        styles=os.listdir(self.indir)
+        for stid in styles:
+            inf=open('{0}/{1}/beers.html'.format(self.indir,stid))
+            stl=inf.read().split('<title>')[-1].split('</title>')[0].split('|')[0]
+            idwrt.writerow([int(stid),stl])
+            inf.close()
+        outf.close()
+    
+    def beer_brew_map(self,beerf='beer_id',brewf='brew_id'):
+        '''
+        mapping beer and brewery IDs
+        '''
+        brset=set()
+        bef=open('{0}/{1}.csv'.format(self.outdir,beerf),'w')
+        brf=open('{0}/{1}.csv'.format(self.outdir,brewf),'w')
+        bewrt=csv.writer(bef,delimiter=';')
+        brwrt=csv.writer(brf,delimiter=';')
+        styles=os.listdir(self.indir)
+        for stid in styles:
+            beers=os.listdir('{0}/{1}'.format(self.indir,stid))
+            for beid in beers:
+                if beid=='beers.html': continue
+                brid=int(beid.split('_')[0])
+                inf=open('{0}/{1}/{2}/page_1.html'.format(self.indir,stid,beid))
+                conts=inf.read().split('<title>')[-1].split('</title>')[0].split('|')[:2]
+                bewrt.writerow([beid,conts[0]])
+                if brid not in brset:
+                    brset.add(brid)
+                    brwrt.writerow([brid,conts[1]])
+                inf.close()
+        bef.close()
+        brf.close()
