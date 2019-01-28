0bf22704fab2d18782127c1f1c779bd62df2093b
Yanchu Wang <yanchu@cern.ch>
2019-01-27 23:54:34 -0500

    matching beeradvocat beers and beermenus beers using google search

---
+++ beermenus_scraper/matcher_google.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import csv, time
+import numpy as np
+from selenium import webdriver
+
+keystr='href="https://www.beeradvocate.com/beer/profile/'
+def one_beer(drv,beername):
+    sbm=drv.find_element_by_name("q")
+    sbm.clear()
+    sbm.send_keys(beername+' beeradvocate')
+    sbm.submit()
+    time.sleep(np.random.normal(5,2.5))
+    cont=drv.page_source
+    if keystr not in cont:
+        print(beername)
+    else:
+        return cont.split(keystr)[1].split('"')[0]
+
+drv=webdriver.Firefox()
+drv.get("https://www.google.com/")
+
+innames=set(pd.read_csv("bars.csv",delimiter=';')['beer'])
+dones=set(pd.read_csv('name_id_matcher.csv',delimiter=';')['name'])
+with open('name_id_matcher.csv','a+') as f:
+    wrt=csv.writer(f,delimiter=';')
+    wrt.writerow(["name",'id'])
+    for i in innames:
+        if i in dones:
+            print('skipping ',i)
+            continue
+        fail=True
+        while fail:
+            fail=False
+            try:
+                outp=one_beer(drv,i)
+            except Exception as e:
+                print(e)
+                time.sleep(30)
+                fail=True
+        if outp:
+            wrt.writerow([i,outp])
+drv.quit()        
+    
